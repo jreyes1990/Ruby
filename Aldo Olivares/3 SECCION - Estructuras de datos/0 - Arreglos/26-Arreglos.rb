d47a5664924e8221941c780a556899a3fb93d954
jreyes1990 <jarl1990mt@gmail.com>
2024-03-16 22:17:31 -0600

    26. Arreglos

---
+++ Aldo Olivares/3 SECCION - Estructuras de datos/0 - Arreglos/26-Arreglos.rb
@@ -0,0 +1,15 @@
+puts("\n***** ARREGLOS *****")
+videoJuegos = ["Zelda", "Mario", "Smash"]
+
+puts("Imprimiendo arreglos: #{videoJuegos}")
+
+puts("\n***** ARREGLO VACIO *****")
+arregloVacio1 = []
+arregloVacio2 = Array.new
+
+puts("Arreglo vacio 1: #{arregloVacio1}\nArreglo Vacio 2: #{arregloVacio2}")
+
+puts("\n***** ARREGLO CON N REPETICIONES *****")
+arregloRepeticiones = Array.new(3, "Mario Smash")
+
+puts("Arreglo: #{arregloRepeticiones}")
