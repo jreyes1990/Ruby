1fa420959d61ceceb4b506944b66f7ff70130e3a
jreyes1990 <jarl1990mt@gmail.com>
2024-03-21 23:28:04 -0600

    54. La Clase List II

--- Aldo Olivares/6 SECCION - Proyecto lista de compras/shopping_app/list.rb
+++ Aldo Olivares/6 SECCION - Proyecto lista de compras/shopping_app/list.rb
@@ -1,3 +1,5 @@
+require "./item.rb"
+
 class List
   attr_writer :items
 
@@ -5,8 +7,8 @@ class List
     @items = Array.new()
   end
 
-  def add_item(texto)
-    item = Item.new
+  def add_item(text)
+    item = Item.new(text)
     @items.push(item)
   end
 
@@ -21,4 +23,58 @@ class List
   def remove_all()
     @items.clear
   end
+
+  def show_all()
+    if @items.length == 0
+      puts("No hay articulos en tu lista de compras")
+    else
+      @items.each_index do |index|
+        puts(index.to_s + " - " + @items[index].to_s)
+      end
+    end
+  end
 end
+
+list = List.new
+
+puts("==================================================================")
+puts("Verificando si existe articulos")
+puts("\n")
+list.show_all
+
+puts("\n")
+list.add_item("Jamon")
+puts("==================================================================")
+puts("Verificando los articulos, despues de agregar un item")
+puts("\n")
+list.show_all
+
+puts("\n")
+list.add_item("Huevos")
+puts("==================================================================")
+puts("Verificando los articulos, despues de agregar un item")
+puts("\n")
+list.show_all
+
+puts("\n")
+list.check_item(1)
+puts("==================================================================")
+puts("Verificando los articulos, despues de un checked al item")
+puts("\n")
+list.show_all
+
+puts("\n")
+list.remove_item(1)
+puts("==================================================================")
+puts("Verificando los articulos, despues de remover el item")
+puts("\n")
+list.show_all
+
+puts("\n")
+list.remove_all
+puts("==================================================================")
+puts("Verificando los articulos, despues de remover todos los items")
+puts("\n")
+list.show_all
+
+puts("\n")
