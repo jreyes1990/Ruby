a0d46ffce60f320dce4defd6d3fe48df02575613
jreyes1990 <jarl1990mt@gmail.com>
2024-03-24 21:07:20 -0600

    76. Routes en Sinatra

--- Aldo Olivares/8 SECCION - Sinatra/talleres/talleres.rb
+++ Aldo Olivares/8 SECCION - Sinatra/talleres/talleres.rb
@@ -1,5 +1,14 @@
 require "sinatra"
 
 get "/" do
-  "Hola mundo"
+  "
+  <h1>Talleres del ciclo 2017-2018</h1>
+  <p>Futbol</P>
+  "
+end
+
+get "/imagenes" do
+  "
+  <h1>Imagenes del sitio web</h1>
+  "
 end
